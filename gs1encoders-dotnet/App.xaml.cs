699e7d006603388508708c1391cabb364b4e204c
Terry Burton <tez@terryburton.co.uk>
2021-05-09 03:40:44 +0100

    C# .NET WPF app using pinvoke wrapper for gs1encoders.dll

---
+++ gs1encoders-dotnet/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace gs1encoders_dotnet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        GS1Encoder gs1Encoder = new GS1Encoder();
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Title = "GS1 Encoders | Library release: " + gs1Encoder.getVersion();
+            mw.Show();
+        }
+
+
+    }
+
+}
---
+++ gs1encoders-dotnet/GS1Encoder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace gs1encoders_dotnet
+{
+    class GS1Encoder
+    {
+
+        private IntPtr ctx;
+        private const String gs1_dll = "gs1encoders.dll";
+
+        /*
+         *  Functions imported from gs1encoders.dll
+         * 
+         */
+        [DllImport(gs1_dll, EntryPoint = "gs1_encoder_init", CallingConvention = CallingConvention.Cdecl)]
+        private static extern System.IntPtr gs1_encoder_init();
+
+        [DllImport(gs1_dll, EntryPoint = "gs1_encoder_getVersion", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr gs1_encoder_getVersion(IntPtr ctx);
+
+        [DllImport(gs1_dll, EntryPoint = "gs1_encoder_getSym", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int gs1_encoder_getSym(IntPtr ctx);
+
+        [DllImport(gs1_dll, EntryPoint = "gs1_encoder_setSym", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void gs1_encoder_setSym(IntPtr ctx, int sym);
+
+        [DllImport(gs1_dll, EntryPoint = "gs1_encoder_free", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void gs1_encoder_free(IntPtr ctx);
+
+        /*
+         *  Methods to provide an OO wrapper around the imported functional interface and to perform marshalling
+         * 
+         */
+        public GS1Encoder()
+        {
+            ctx = gs1_encoder_init();
+        }
+
+        ~GS1Encoder()
+        {           
+            gs1_encoder_free(ctx);
+        }
+
+        public string getVersion()
+        {           
+            return System.Runtime.InteropServices.Marshal.PtrToStringAnsi(gs1_encoder_getVersion(ctx));           
+        }
+
+        public void setSym(int sym)
+        {
+            gs1_encoder_setSym(ctx, sym);
+        }
+
+        public int getSym()
+        {
+            return gs1_encoder_getSym(ctx);
+        }        
+
+    }
+}
