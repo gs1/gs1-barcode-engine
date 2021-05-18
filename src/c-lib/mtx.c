3afa16ebe240ddd48fd2d4a5dd937271c1a6d9e6
Terry Burton <tez@terryburton.co.uk>
2021-05-18 21:11:05 +0100

    Matrix x,y bit getter and setter and conversion to patterns using RLE

---
+++ src/c-lib/mtx.c
@@ -0,0 +1,71 @@
+/**
+ * GS1 barcode encoder application
+ *
+ * @author Copyright (c) 2000-2020 GS1 AISBL.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <stdint.h>
+
+#include "mtx.h"
+
+
+void gs1_mtxPutBit(uint8_t *mtx, int cols, int x, int y, uint8_t bit) {
+
+	int p = ((cols-1)/8+1)*y + x/8;
+
+	if (bit != 0) {
+		mtx[p] = (uint8_t)(mtx[p] | (0x80 >> x%8));
+	}
+	else {
+		mtx[p] = (uint8_t)(mtx[p] & (~(0x80 >> x%8)));
+	}
+
+}
+
+
+int gs1_mtxGetBit(uint8_t *mtx, int cols, int x, int y) {
+
+	int p = ((cols-1)/8+1)*y + x/8;
+
+	return (mtx[p] & (0x80 >> x%8)) != 0 ? 1:0;
+
+}
+
+
+// Runlength encode the matrix to a set of patterns
+void gs1_mtxToPatterns(uint8_t* mtx, int cols, int rows, struct patternLength *pats) {
+
+	int r, c, patPos, last;
+
+	for (r = 0; r < rows; r++) {
+		patPos = 0;
+		last = gs1_mtxGetBit(mtx, cols, 0, r);
+		pats[r].whtFirst = (last == 0);
+		pats[r].pattern[0] = 1;
+		for (c = 1; c < cols; c++) {
+			if (gs1_mtxGetBit(mtx, cols, c, r) == last) {
+				pats[r].pattern[patPos]++;
+			}
+			else {
+				pats[r].pattern[++patPos] = 1;
+				last ^= 1;
+			}
+		}
+		pats[r].length = (uint8_t)patPos+1;
+	}
+
+}
---
+++ src/c-lib/mtx.h
@@ -0,0 +1,42 @@
+/**
+ * GS1 barcode encoder application
+ *
+ * @author Copyright (c) 2000-2020 GS1 AISBL.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef MTX_H
+#define MTX_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+
+#define MAX_MTX_BYTES	32568;
+
+
+struct patternLength {
+	bool whtFirst;
+	uint8_t pattern[200];  // Sufficient for MAX_QR_ELMNTS
+	uint8_t length;
+};
+
+void gs1_mtxPutBit(uint8_t *mtx, int cols, int x, int y, uint8_t bit);
+int gs1_mtxGetBit(uint8_t *mtx, int cols, int x, int y);
+void gs1_mtxToPatterns(uint8_t* mtx, int w, int h, struct patternLength *pats);
+
+
+#endif  /* MTX_H */
