bf867c6789862dfe1fb4e8d172112da7adb7c0cf
Terry Burton <tez@terryburton.co.uk>
2021-05-24 01:59:58 +0100

    DM: RSEC

--- src/c-lib/dm.h
+++ src/c-lib/dm.h
@@ -21,11 +21,16 @@
 #ifndef DM_H
 #define DM_H
 
+#define DM_QZ		1
+#define MAX_DM_COLS	(144 + 2*DM_QZ)
+#define MAX_DM_ROWS	(144 + 2*DM_QZ)
+#define MAX_DM_ELMNTS	MAX_DM_COLS		// to accept checkerboard pattern
+#define MAX_DM_BYTES	(((MAX_DM_COLS-1)/8+1) * MAX_DM_ROWS)
 
-#define MAX_DM_COLS	146     // include qz
-#define MAX_DM_ROWS	146     // include qz
-#define MAX_DM_ELMNTS	146     // accept checkerboard pattern
-#define MAX_DM_BYTES	2774
+#define MAX_DM_CWS	2178
+
+#define MAX_DM_DAT_CWS_PER_BLK 175
+#define MAX_DM_ECC_CWS_PER_BLK 68
 
 
 #include "gs1encoders.h"
